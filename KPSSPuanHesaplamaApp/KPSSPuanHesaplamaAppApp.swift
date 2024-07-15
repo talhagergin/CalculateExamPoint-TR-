7dd120446df8df596b5c73c9a04c5a68864f99df
Talha Gergin <talhagergin6@gmail.com>
2024-07-15 16:51:19 +0300

    Initial Commit

---
+++ KPSSPuanHesaplamaApp/KPSSPuanHesaplamaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPSSPuanHesaplamaAppApp.swift
+//  KPSSPuanHesaplamaApp
+//
+//  Created by Talha Gergin on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct KPSSPuanHesaplamaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
